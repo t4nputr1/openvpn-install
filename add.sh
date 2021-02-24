cb1adebf86315bc0264ff6a3869bc815d6e96449
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-02-24 21:36:10 +0700

    Update add.sh

--- add.sh
+++ add.sh
@@ -19,7 +19,7 @@ saat_expired=$(($today + $masa_aktif_detik))
 tanggal_expired=$(date -u --date="1970-01-01 $saat_expired sec GMT" +%Y/%m/%d)
 tanggal_expired_display=$(date -u --date="1970-01-01 $saat_expired sec GMT" '+%d %B %Y')
 
-export MENU_OPTION="1"; export CLIENT="$username"; export PASS="$password"; bash /root/openvpn-install.sh &> /dev/null;
+export MENU_OPTION="1"; export CLIENT="$username"; export PASS="$password"; bash openvpn-install.sh &> /dev/null;
 
 mv $username.ovpn /etc/openvpn/client
 cp /etc/openvpn/client/$username.ovpn /home/vps/public_html/client
@@ -27,12 +27,14 @@ clear
 
 cat <<EOF
 =================================
-DETAIL AKUN OPENVPN
+      DETAIL AKUN OPENVPN
 ---------------------------------
-Server IP    : $MYIP
-Username     : $username
-Password     : $password
-Aktif Sampai : $tanggal_expired_display
-Config       : http://MYIP:81/client/$username.ovpn
+Host/IP   : $MYIP
+Username  : $username
+Password  : $password
+Port      : 443
+Squid     : 80, 8000, 8080, 3128
+Aktif s/d : $tanggal_expired_display
+Config    : http://$MYIP:81/client/$username.ovpn
 =================================
 EOF
