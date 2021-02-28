1463b1b4334618644178d8f2993b39506dee3224
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-02-28 23:31:37 +0700

    Update and rename add.sh to create-account.sh

--- create-account.sh
+++ create-account.sh
@@ -11,13 +11,10 @@ MYIP2="s/xxxxxxxxx/$MYIP/g";
 echo ""
 read -p "Isikan Client User: " username
 read -p "Isikan password [$username]: " password
-read -p "Berapa hari akun [$username] aktif: " masa_aktif
+read -p "Berapa hari account [$username] aktif: " AKTIF
 
-today=`date +%s`
-masa_aktif_detik=$(( $masa_aktif * 86400 ))
-saat_expired=$(($today + $masa_aktif_detik))
-tanggal_expired=$(date -u --date="1970-01-01 $saat_expired sec GMT" +%Y/%m/%d)
-tanggal_expired_display=$(date -u --date="1970-01-01 $saat_expired sec GMT" '+%d %B %Y')
+today="$(date +"%Y-%m-%d")"
+expire=$(date -d "$AKTIF days" +"%Y-%m-%d")
 
 export MENU_OPTION="1"; export CLIENT="$username"; export PASS="$password"; bash openvpn-install.sh &> /dev/null;
 
@@ -31,7 +28,7 @@ Host/IP   : $MYIP
 Username  : $username
 Password  : $password
 Port      : 443
-Aktif s/d : $tanggal_expired_display
+Aktif s/d : $(date -d "$AKTIF days" +"%d-%m-%Y")
 =================================
 Config    :  /root/$username.ovpn
 EOF
