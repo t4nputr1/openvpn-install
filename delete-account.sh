a6377c01cc534cfa7589f617b9c4f91bb98ff0e1
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-03-01 00:43:27 +0700

    Update delete-account.sh

--- delete-account.sh
+++ delete-account.sh
@@ -6,23 +6,16 @@ if [[ $USER != 'root' ]]; then
 fi
 clear
 
-echo "-----------------------------------"
-echo "USERNAME              EXP DATE     "
-echo "-----------------------------------"
-
-while read expired
-do
-	AKUN="$(echo $expired | cut -d: -f1)"
-	ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
-	exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
-	if [[ $ID -ge 1000 ]]; then
-		printf "%-21s %2s\n" "$AKUN" "$exp"
-	fi
-done < /etc/openvpn/akun.conf
-echo "-----------------------------------"
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/openvpn/akun.conf")
+echo ""
+echo "Input the existing client you want to revoke"
+grep -E "^### " "/etc/v2ray/akun.conf" | cut -d ' ' -f 2-3 | nl -s ') '
 read -p "Enter username to delete: " username
 
-egrep "^$username" /etc/openvpn/akun.conf >/dev/null
+# match the selected number to a client name
+CLIENT_NAME=$(grep -E "^### " "/etc/v2ray/akun.conf" | cut -d ' ' -f 2-3 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/etc/v2ray/akun.conf" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $CLIENT_NAME\$/,/^$/d" "/etc/openvpn/akun.conf"
 
 cd /etc/openvpn/easy-rsa/ || return; ./easyrsa --batch revoke "$username"; EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl; rm -f /etc/openvpn/crl.pem; cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem; chmod 644 /etc/openvpn/crl.pem; find /home/ -maxdepth 2 -name "$username.ovpn" -delete; rm -f "/root/$username.ovpn"; sed -i "/^$username,.*/d" /etc/openvpn/ipp.txt; /dev/null;
 service openvpn restart
