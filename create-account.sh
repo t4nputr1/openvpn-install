bdfb6a574eca5af2ffbf8f4f3bff7008509ef382
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-03-01 00:39:48 +0700

    Update create-account.sh

--- create-account.sh
+++ create-account.sh
@@ -10,15 +10,23 @@ echo -e "\n " >> /etc/openvpn/akun.conf
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 echo ""
-read -p "Isikan Client User: " username
-egrep "^$username" /etc/openvpn/akun.conf >/dev/null
+
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -p "Isikan Client User: " username
+		CLIENT_EXISTS=$(grep -c -E "^### $username\$" "/etc/openvpn/akun.conf")
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "Client [$username] is available , try another Username"
+			echo ""
+		fi
+	done
 read -p "Isikan password [$username]: " password
 read -p "Berapa hari account [$username] aktif: " AKTIF
 
 today="$(date +"%Y-%m-%d")"
 expire=$(date -d "$AKTIF days" +"%Y-%m-%d")
-useradd -M -N -s /bin/false -e $expire $username
-echo $username:$password | chpasswd
+echo -e "\n### $username $(date -d "$AKTIF days" +"%d-%m-%Y")">>"/etc/openvpn/akun.conf"
 
 export MENU_OPTION="1"; export CLIENT="$username"; export PASS="$password"; bash openvpn-install.sh &> /dev/null;
 
